59b53129b42ee5241b035b5c6f0c58cbe7c7a1c2
hlwqds <545743488@qq.com>
2020-01-11 09:43:33 +0800

    golang

--- golang的再学习/practice/1/hello.go
+++ golang的再学习/practice/1/hello.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+type Person struct{
+	Name string
+	Age int
+}
+
+type Student struct{
+	*Person
+	Number int
+}
+
+
 func main(){
-	fmt.Printf("Hello, world!\n")
+	p := &Person{
+		Name: "huanglin",
+		Age: 24,
+	}
+
+	s := Student{
+		Person: p,
+		Number: 110,
+	}
+	fmt.Printf("%s\n",s.Name)
+
+	L1:
+		fmt.Println("L1")
+		return
+
+
+	if true{
+		fmt.Println("haha")
+		goto L1
+	}
+
 }
