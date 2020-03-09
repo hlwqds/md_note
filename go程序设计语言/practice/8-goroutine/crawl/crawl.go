3f8df5bfbf32aed0c69880d7ed0fe86aeb3f5966
hlwqds <50267463+hlwqds@users.noreply.github.com>
2020-03-09 18:07:55 +0800

    Update crawl.go

--- go程序设计语言/practice/8-goroutine/crawl/crawl.go
+++ go程序设计语言/practice/8-goroutine/crawl/crawl.go
@@ -3,15 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/my/repo/go程序设计语言/practice/8-goroutine/links"
 
 )
 
-func cral(url string) []string {
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
 	fmt.Println(url)
+	tokens <- struct{}{}
 	list, err := links.Extract(url)
+	<-tokens
 	if err != nil {
 		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+	go func() { worklist <- os.Args[1:] }()
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
 	}
 }
