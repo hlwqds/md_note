a216a5dadcbe5fb0e825a724115dc6226d1e3fff
ubuntu <ubuntu@localhost.localdomain>
2020-03-18 08:56:14 +0800

    up

--- c程序设计语言/导言/getLongestLine.c
+++ c程序设计语言/导言/getLongestLine.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAXLINE 1000    //允许的输入行的最大长度
 
-int getLine(char line[], int maxline);
+int getLine(char **line);
 void copy(char to[], char from[]);
-
+#define FREE_IF_NOT_NULL(x) {if(x){free(x);x=NULL;}}
 int main(){
     int len = 0;
     int max = 0;
-    char line[MAXLINE] = {0};
-    char longest[MAXLINE] = {0};
+    char *line = NULL;
+    char *longest = NULL;
 
-    while((len = getLine(line, MAXLINE)) > 0){
+    while((len = getLine(&line)) > 0){
         if(len > max){
             max = len;
-            copy(longest, line);
-        }
+			FREE_IF_NOT_NULL(longest);
+			longest = line;
+        }else{
+			FREE_IF_NOT_NULL(line);
+		}
     }
     if(max > 0){
         printf("%s:len-%d\n", longest, max);
     }
+	FREE_IF_NOT_NULL(longest);
+	FREE_IF_NOT_NULL(line);
     return 0;
 }
 
@@ -30,15 +36,23 @@ void copy(char to[], char from[]){
 }
 
 //getline函数，将一行读入s中并返回其长度
-int getLine(char line[], int maxline){
-    int c, i;
-    for(i = 0; i<maxline-1 && (c = getchar())!=EOF && c!='\n'; i++){
-        line[i] = c;
+int getLine(char **line){
+    int c, i = 0, size = 0;
+	size = 16;
+	*line = malloc(sizeof(char) * size);
+	while((c = getchar())!=EOF && c!='\n'){
+		i++;
+		if(i >= size){
+			size *= 2;
+			*line = realloc(*line, sizeof(char) * size);
+		}
+        (*line)[i-1] = c;
     }
 
     if (c == '\n'){
         i++;
     }
-    line[i] = '\0';
+    (*line)[i] = '\0';
     return i;
 }
+
