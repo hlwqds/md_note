f22e6df8269c5b12bf3ee26c55661df7f0f20cd0
hlwqds <545743488@qq.com>
2020-03-25 08:31:03 +0800

    quicksort

--- 算法图解/goRealization/quickSort/quickSort.go
+++ 算法图解/goRealization/quickSort/quickSort.go
@@ -1,12 +1,31 @@
 package main
 
+import "fmt"
+
 func quickSort(list []int) {
 	length := len(list)
 	if length <= 1 {
 		return
 	} else {
-		var cond *int
+		last := 1
 		base := length / 2
+		list[0], list[base] = list[base], list[0]
+		for i := 1; i < length; i++ {
+			if list[i] < list[last] {
+				list[i], list[last] = list[last], list[i]
+				last++
+				list[i], list[last] = list[last], list[i]
+			}
+			list[0], list[last-1] = list[last-1], list[0]
+		}
 
+		quickSort(list[0 : last-1])
+		quickSort(list[last:])
 	}
+}
+
+func main() {
+	list := []int{4, 2, 6, 8, 1, 2, 8}
+	quickSort(list)
+	fmt.Println(list)
 }
