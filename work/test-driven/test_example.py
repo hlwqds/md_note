6d69c82cac291b212fb4f237fd31bc57734cce0f
hlwqds <545743488@qq.com>
2020-04-01 10:50:37 +0800

    test up

---
+++ work/test-driven/test_example.py
@@ -0,0 +1,22 @@
+from test_mysql import *
+from test_udp import *
+
+if __name__ == "__main__":
+    test_init_one_thousand_customer()
+    test_customer_export()
+    #time.sleep(1)
+    #test_preview_call_batch_export()
+    #time.sleep(1)
+    #test_preview_call_export()
+    #time.sleep(1)
+    #test_preview_call_detail_export()
+    #time.sleep(1)
+    #test_preview_call_cm_result_export()
+    #time.sleep(1)
+    #test_preview_call_seat_statis_export()
+    #time.sleep(1)
+    #test_preview_call_group_statis_export()
+    #time.sleep(1)
+    #test_preview_call_batch_export()
+    #time.sleep(1)
+    #test_preview_call_statis_export()
